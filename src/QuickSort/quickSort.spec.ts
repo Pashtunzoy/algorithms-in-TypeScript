bd847b087e9f0e7fb7d1cf8ac55ec0e907a11526
pashtunzoy <asifkhannoorzai@gmail.com>
2018-02-22 23:51:37 +1100

    Implmented the QuickSort algorithm using TDD workflow

--- src/QuickSort/quickSort.spec.ts
+++ src/QuickSort/quickSort.spec.ts
@@ -6,13 +6,13 @@ test('Export a function', function (t) {
     t.end();
 });
 
-test('Takes three arguments', function (t) {
-    t.equal(quickSort.length, 3);
+test('Takes one arguments', function (t) {
+    t.equal(quickSort.length, 1);
     t.end();
 });
 
-// test('Should return the array sorted', function (t) {
-//     var array = [2, 3, 4, 5, 1, 6];
-//     t.equal(quickSort(array, 2, 5), [1, 2, 3, 4, 5, 6]);
-//     t.end();
-// });
+test('Should return the array sorted', function (t) {
+    var array = [2, 3, 4, 5, 1, 6];
+    t.deepEqual(quickSort(array, 2, 5), [1, 2, 3, 4, 5, 6]);
+    t.end();
+});
--- src/QuickSort/quickSort.ts
+++ src/QuickSort/quickSort.ts
@@ -1,7 +1,13 @@
-export default function quickSort(array: any[], low: any, high: any): any[] {
-    let pivot: any = array[high];
-    if (low < pivot) {
-        return pivot;
+export default function quickSort(arr: any[]): any[] {
+    if(arr.length < 2) {
+        return arr;
+    } else {
+        let low = [];
+        let high = [];
+        for (let i = 1; i < arr.length; i += 1) {
+            if (arr[i] < arr[0]) low.push(arr[i]);
+            if(arr[i] >= arr[0]) high.push(arr[i]);
+        }
     }
-    return array;
+    return quickSort(low).concat(arr[0], quickSort(high));
 }
