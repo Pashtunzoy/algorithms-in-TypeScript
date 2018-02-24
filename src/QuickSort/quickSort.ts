3dfea0b0c3f25a258a24f6b23d0dbc67aefb40d0
pashtunzoy <asifkhannoorzai@gmail.com>
2018-02-24 21:27:04 +1100

    Choose the middle value instead of the first to get average case of the algorithm, all the time

--- src/QuickSort/quickSort.ts
+++ src/QuickSort/quickSort.ts
@@ -1,13 +1,16 @@
 export default function quickSort(arr: any[]): any[] {
     if(arr.length < 2) {
         return arr;
-    } else {
-        let low = [];
-        let high = [];
-        for (let i = 1; i < arr.length; i += 1) {
-            if (arr[i] < arr[0]) low.push(arr[i]);
-            if(arr[i] >= arr[0]) high.push(arr[i]);
+    }
+    let low = [];
+    let high = [];
+    let pivot = Math.floor((arr.length-1)/2);
+    for (let i = 0; i < arr.length; i += 1) {
+        if(arr[i] !== arr[pivot]) {
+            if (arr[i] < arr[pivot]) low.push(arr[i]);
+            if(arr[i] >= arr[pivot]) high.push(arr[i]);
         }
     }
-    return quickSort(low).concat(arr[0], quickSort(high));
+    return quickSort(low).concat(arr[pivot], quickSort(high));
 }
+
