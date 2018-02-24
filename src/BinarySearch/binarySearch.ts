1809fe6f5b3012f1cab0f2d9ba227d76a9007f6e
pashtunzoy <asifkhannoorzai@gmail.com>
2018-02-24 23:25:13 +1100

    Made the binarySearch algorithm Recursive instead of a while loop

--- src/BinarySearch/binarySearch.ts
+++ src/BinarySearch/binarySearch.ts
@@ -1,17 +1,21 @@
-export default function binarySearch(arr: number[] | string[], item: number|string): null|number {
-    let low = 0;
-    let high: number = arr.length - 1;
+export default function binarySearch(arr: number[] | string[], item: number|string, low: any = 0, high: any = 0) {
+    if (arr.length < 2) {
+        return null;
+    }
 
-    while (low <= high) {
+    high = arr.length - 1;
+    if (low <= high) {
         let mid = Math.floor((low + high) / 2);
         let guess = arr[mid];
+       
         if (guess === item) {
             return mid;
-        } else if (guess > item && arr.length > 1) {
-            high = mid - 1;
-        } else {
-            low = mid + 1;
+        } else if (guess > item) {
+            arr.splice(mid, 1);
+            return binarySearch(arr, item, low, mid - 1);     
         }
+        arr.splice(mid, 1);
+        return binarySearch(arr, item, mid + 1, high);
     }
     return null;
 }
