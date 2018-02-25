80604eef9976a40d55050a57307fb32b9eccde87
pashtunzoy <asifkhannoorzai@gmail.com>
2018-02-25 22:16:04 +1100

    WIP of the breatFirstSearch algorithm

--- src/BreadthFirstSearch/breadthFirstSearch.spec.ts
+++ src/BreadthFirstSearch/breadthFirstSearch.spec.ts
@@ -5,3 +5,16 @@ test('Export a function', function (t) {
     t.equal(typeof breadthFirstSearch, 'function');
     t.end();
 });
+
+
+test('Return the correct path as array value for the searched term', function (t) {
+    const data = [
+        {
+            value: "Dandenong",
+            children: [1, 3, 4],
+        }
+    ];
+
+    t.equal(breadthFirstSearch(data, "Dovton"), []);
+    t.end();
+});
--- src/BreadthFirstSearch/breadthFirstSearch.ts
+++ src/BreadthFirstSearch/breadthFirstSearch.ts
@@ -1,3 +1,16 @@
-export default function breadFirstSearch() {
-    return;
+interface DataItem {
+        parentNode?: number;
+        value: string;
+        children: number[];
+}
+interface DataItems extends Array<DataItem>{};
+
+export default function breadFirstSearch(data: DataItems, term: string) {
+    let alreadySearched: string[];
+    let finalResult: object[];
+    let queue = [];
+    for (let i = 0, len = data.length; i < len; i += 1) {
+        // TODO
+    }
+    return finalResult.length > 1 ? finalResult : finalResult[0];
 }
